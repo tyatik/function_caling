740b8edac2a3c4d7c2396422baf14cba768abe28
Ilya Korotkov <i59korotkov@gmail.com>
2024-04-08 12:19:41 +0300

    Changed data download and format scripts

--- src/utils/data/download_dataset.py
+++ src/utils/data/download_dataset.py
@@ -1,16 +1,7 @@
-import json
 import argparse
-from collections import defaultdict
-
 import datasets
-from tqdm import tqdm
 
-import src.utils.data.formats as formats
-
-FORMAT_TO_FUNC = {
-    "raw": lambda messages, functions: json.dumps({"messages": messages, "functions": functions}),
-    "llama": formats.llama_convert
-}
+from src.utils.data.formats import convert_dataset
 
 
 if __name__ == "__main__":
@@ -19,25 +10,14 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--format", type=str, default="llama", help="Dataset format")
     args = parser.parse_args()
 
-    if args.format not in FORMAT_TO_FUNC:
-        raise ValueError(f"Format must be one of: {list(FORMAT_TO_FUNC.keys())}, got '{args.format}'")
-
-    # Download raw hugginface dataset
+    # Download raw huggingface dataset
     hf_dataset = datasets.load_dataset("korotkov/glaive-function-calling-v2-parsed")
 
-    # Convert dataset to another format
-    converted_dataset = datasets.DatasetDict()
-    for part in ["train", "test"]:
-        converted_dataset[part] = defaultdict(list)
+    # Convert dataset
+    converted_dataset = convert_dataset(
+        dataset=hf_dataset,
+        format=args.format,
+    )
 
-        for row in tqdm(hf_dataset[part]):
-            messages = json.loads(row["messages"])
-            functions = json.loads(row["functions"])
-
-            for feature, value in FORMAT_TO_FUNC[args.format](messages, functions).items():
-                converted_dataset[part][feature].append(value)
-        
-        converted_dataset[part] = datasets.Dataset.from_dict(converted_dataset[part])
-
-    # Save converted dataset
+    # Save dataset to disk
     converted_dataset.save_to_disk(args.path)
--- src/utils/data/formats/__init__.py
+++ src/utils/data/formats/__init__.py
@@ -1,1 +1,33 @@
+import json
+from collections import defaultdict
+
+import datasets
+from tqdm import tqdm
+
 from .llama import convert as llama_convert
+
+FORMAT_TO_FUNC = {
+    "raw": lambda messages, functions: json.dumps({"messages": messages, "functions": functions}),
+    "llama": llama_convert
+}
+
+
+def convert_dataset(dataset, format: str):
+    if format not in FORMAT_TO_FUNC:
+        raise ValueError(f"Format must be one of: {list(FORMAT_TO_FUNC.keys())}, got '{format}'")
+
+    # Convert dataset to another format
+    converted_dataset = datasets.DatasetDict()
+    for part in ["train", "test"]:
+        converted_dataset[part] = defaultdict(list)
+
+        for row in tqdm(dataset[part]):
+            messages = json.loads(row["messages"])
+            functions = json.loads(row["functions"])
+
+            for feature, value in FORMAT_TO_FUNC[format](messages, functions).items():
+                converted_dataset[part][feature].append(value)
+        
+        converted_dataset[part] = datasets.Dataset.from_dict(converted_dataset[part])
+    
+    return converted_dataset
